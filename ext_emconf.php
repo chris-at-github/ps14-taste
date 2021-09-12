729765dd01ef0cc30f3de5dca5cbd588863d9e6b
Christian Pschorr <c.pschorr@datamints.com>
2021-09-12 22:24:25 +0200

    Abhängigkeit auf XO hinterlegt

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,19 +11,20 @@
  ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Ps14 Taste',
-    'description' => '',
-    'category' => 'plugin',
-    'author' => 'Christian Pschorr',
-    'author_email' => 'pschorr.christian@gmail.com',
-    'state' => 'alpha',
-    'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
-    'constraints' => [
-        'depends' => [
-            'typo3' => '10.4.0-10.4.99',
-        ],
-        'conflicts' => [],
-        'suggests' => [],
-    ],
+	'title' => 'Ps14 Taste',
+	'description' => '',
+	'category' => 'plugin',
+	'author' => 'Christian Pschorr',
+	'author_email' => 'pschorr.christian@gmail.com',
+	'state' => 'alpha',
+	'clearCacheOnLoad' => 0,
+	'version' => '1.0.0',
+	'constraints' => [
+		'depends' => [
+			'typo3' => '10.4.0-10.4.99',
+			'xo' => '1.0.0-1.9.99',
+		],
+		'conflicts' => [],
+		'suggests' => [],
+	],
 ];
