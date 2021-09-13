6268879e602ac1151f950b8999b7f4fc796deeac
Christian Pschorr <c.pschorr@datamints.com>
2021-09-13 22:26:38 +0200

    Einbau der Felder AbstractLong und Badges und Auslesen der Badges per Konfiguration

--- Classes/Domain/Model/Page.php
+++ Classes/Domain/Model/Page.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 namespace Ps14\Taste\Domain\Model;
 
 
+use Ps\Xo\Domain\Model\Category;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
+
 /**
  * This file is part of the "Ps14 Taste" Extension for TYPO3 CMS.
  *
@@ -18,34 +22,65 @@ namespace Ps14\Taste\Domain\Model;
 /**
  * Page
  */
-class Page extends \Ps\Xo\Domain\Model\Page
-{
+class Page extends \Ps\Xo\Domain\Model\Page {
 
-    /**
-     * txTasteAbstractLong
-     *
-     * @var string
-     */
-    protected $txTasteAbstractLong = '';
+	/**
+	 * @var string
+	 */
+	protected $abstractLong = '';
 
-    /**
-     * Returns the txTasteAbstractLong
-     *
-     * @return string $txTasteAbstractLong
-     */
-    public function getTxTasteAbstractLong()
-    {
-        return $this->txTasteAbstractLong;
-    }
+	/**
+	 * @var array
+	 */
+	protected $badges = [];
 
-    /**
-     * Sets the txTasteAbstractLong
-     *
-     * @param string $txTasteAbstractLong
-     * @return void
-     */
-    public function setTxTasteAbstractLong(string $txTasteAbstractLong)
-    {
-        $this->txTasteAbstractLong = $txTasteAbstractLong;
-    }
+	/**
+	 * Returns the abstractLong
+	 *
+	 * @return string $abstractLong
+	 */
+	public function getAbstractLong() {
+		return $this->abstractLong;
+	}
+
+	/**
+	 * Sets the abstractLong
+	 *
+	 * @param string $abstractLong
+	 * @return void
+	 */
+	public function setAbstractLong(string $abstractLong) {
+		$this->abstractLong = $abstractLong;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getBadges() {
+		if(empty($this->badges) === true) {
+			$settings = $this->getSettings();
+			$badgeParentCategories = GeneralUtility::intExplode(',', $settings['badgeParentCategories']);
+
+			/** @var Category $category */
+			foreach($this->getCategories() as $category) {
+				if($category->getParent() !== null && in_array($category->getParent()->getUid(), $badgeParentCategories) === true) {
+					$this->badges[] = $category;
+				}
+			}
+		}
+
+		return $this->badges;
+	}
+
+	/**
+	 * liefert die TypoScript Plugin Einstellungen
+	 *
+	 * @return object
+	 */
+	public function getSettings() {
+		return GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Configuration\ConfigurationManager::class)->getConfiguration(
+			\TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface::CONFIGURATION_TYPE_SETTINGS,
+			'taste'
+		);
+	}
 }
--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -1,9 +1,13 @@
 <?php
-declare(strict_types = 1);
+declare(strict_types=1);
 
 return [
-    \Ps14\Taste\Domain\Model\Page::class => [
-        'tableName' => 'pages',
-        
-    ],
+	\Ps14\Taste\Domain\Model\Page::class => [
+		'tableName' => 'pages',
+		'properties' => [
+			'abstractLong' => [
+				'fieldName' => 'tx_taste_abstract_long'
+			],
+		]
+	],
 ];
