de8b1818636ef95c9c577f219231ccb22c7ae292
Christian Pschorr <c.pschorr@datamints.com>
2021-11-14 23:05:28 +0100

    Übersetzungsverhalten von Flexform für Plugins angepasst; Bild auf pages auf l10n=exclude gesetzt; Tech-Teaser liesst nun den TeaserTitle aus

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -7,6 +7,5 @@ defined('TYPO3_MODE') || die();
 	'Teaser Frontend'
 );
 
-
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['teaser_frontend'] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('teaser_frontend', 'FILE:EXT:teaser/Configuration/FlexForms/Frontend.xml');
